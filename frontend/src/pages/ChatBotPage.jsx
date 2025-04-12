a9b8fdde6f52ef532b35ac0ff851e1661d0277ea
Vishal <vishal@akkis-MacBook-Pro.local>
2025-04-12 15:25:43 +0530

    chat bot enhanced

--- frontend/src/pages/ChatBotPage.jsx
+++ frontend/src/pages/ChatBotPage.jsx
@@ -311,9 +311,7 @@ const ChatBotPage = () => {
                 disabled={isLoading || !input.trim()}
                 className="send-button"
               >
-                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
+                Send
               </button>
             </div>
             
